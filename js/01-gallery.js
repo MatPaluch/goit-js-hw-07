b62577b627cf558d93cd89b40107ae6f159d8c8c
MatPaluch <xp.mateusz@gmail.com>
2024-03-28 16:48:07 +0100

    Start

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,7 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const link = document.querySelector(".gallery");
+link.addEventListener("click", (event) => event.preventDefault());
