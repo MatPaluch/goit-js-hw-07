d6a21850fa2a64bbc3495e84ec0e6e6709dd36dc
MatPaluch <xp.mateusz@gmail.com>
2024-03-29 13:28:01 +0100

    Final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,59 @@
 import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
 console.log(galleryItems);
-
 const link = document.querySelector(".gallery");
-link.addEventListener("click", (event) => event.preventDefault());
+
+// for (const obj of galleryItems) {
+//   link.innerHTML += `<div class="gallery__item">
+//   <a class="gallery__link" href="${obj.original}">
+//     <img
+//       class="gallery__image"
+//       src="${obj.preview}"
+//       data-source="${obj.original}"
+//       alt="${obj.description}"
+//     />
+//   </a>
+// </div>`;
+// }
+
+for (const obj of galleryItems) {
+  const div = document.createElement("div");
+  div.classList.add("gallery__item");
+
+  const anchor = document.createElement("a");
+  anchor.classList.add("gallery__link");
+  anchor.setAttribute("href", obj.original);
+
+  const image = document.createElement("img");
+  image.classList.add("gallery__image");
+  image.src = obj.preview;
+  image.setAttribute("data-source", obj.original);
+  image.alt = obj.description;
+
+  link.appendChild(div);
+  div.appendChild(anchor);
+  anchor.appendChild(image);
+}
+
+link.addEventListener("click", showImage);
+
+function showImage(event) {
+  event.preventDefault();
+  basicLightbox
+    .create(`<img src='${event.target.dataset.source}'/>`, {
+      onShow: closeByEsc,
+    })
+    .show();
+}
+
+function closeByEsc(instance) {
+  document.addEventListener("keydown", closeModal);
+
+  function closeModal(event) {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  }
+}
